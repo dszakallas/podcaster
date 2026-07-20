997b35178a3f522fb3b05495d0304b03466ae9ab
Dávid Szakállas <5807322+dszakallas@users.noreply.github.com>
2026-07-20 23:30:09 +0200

    feat: add image build action

--- nix/ai-sdk-anthropic/default.nix
+++ nix/ai-sdk-anthropic/default.nix
@@ -1,5 +1,6 @@
 {
   buildNpmPackage,
+  fetchNpmDeps,
   fetchurl,
   jq,
 }:
@@ -13,14 +14,19 @@ buildNpmPackage rec {
     hash = "sha512-vyH4D6Auih5H2xvVzzh2ep5pbdWiaV7JDC+jHUE7zZJ5Kyv0TteLav4DrOgHzRuyv8ptfUSqFF6Y8//f/Ec0fQ==";
   };
 
-  nativeBuildInputs = [ jq ];
-
   postPatch = ''
     cp ${./package-lock.json} package-lock.json
     jq 'del(.devDependencies)' package.json > package.json.tmp && mv package.json.tmp package.json
   '';
 
-  dontNpmBuild = true;
+  nativeBuildInputs = [ jq ];
 
-  npmDepsHash = "sha256-T9e64cNXfH7nNjdc7jb40AoUvz4Z3g0bi2khqhHoZYk=";
+  npmDeps = fetchNpmDeps {
+    inherit src postPatch;
+    name = "${pname}-${version}-npm-deps";
+    hash = "sha256-T9e64cNXfH7nNjdc7jb40AoUvz4Z3g0bi2khqhHoZYk=";
+    nativeBuildInputs = [ jq ];
+  };
+
+  dontNpmBuild = true;
 }
